71f5880eef93a9dd685bfdc72453b4e0e3ea179f
남쭌 <jjune095@naver.com>
2020-09-27 19:34:22 +0900

    state props, list key

---
+++ src/components/habit.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    //state 오브젝트 안에 있는 count 증가 후 업데이트
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
+  render() {
+    console.log(this.props.habit); //외부에서 받아오는 데이터를 확인 가능.absolute
+    const { name, count } = this.props.habit;
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
+
+export default Habit;
